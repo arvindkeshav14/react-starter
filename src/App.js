304e55ca4a8a1fbec872e16118224992c700892c
arvindkeshav14 <arvindkeshav014@gmail.com>
2022-08-04 09:32:17 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./Demo.css";
+
+function App() {
+  const textVariable = "Welcome to React";
+
+  return (
+    <div className="App">
+      <div className="AnotherClass">
+        <div className="Box">Div One</div>
+        <div className="Box">Div Two</div>
+        <div className="Box">Dive Three</div>
+      </div>
+    </div>
+  );
+}
+export default App;
